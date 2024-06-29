b1eb33ef1c9fd60c3ca194dc8ddb15fd5a4ccc58
mayank0827 <149467189+mayank0827@users.noreply.github.com>
2024-06-29 14:45:21 +0530

    first commit

---
+++ src/Components/Counter.jsx
@@ -0,0 +1,17 @@
+import { useDispatch, useSelector } from "react-redux";
+import { changeValue, decrement, increment } from '../Store/Counter'
+
+const Counter = () => {
+    const { count } = useSelector((state) => state.counter);
+    const disptach = useDispatch();
+
+    return (
+        <div>
+            <button onClick={() => disptach(decrement())}>-</button>
+            <input type="number" value={count} onChange={(e) =>  disptach(changeValue(e.target.value))}/>
+            <button onClick={() => disptach(increment())}>+</button>
+        </div>
+    )
+}
+
+export default Counter;
